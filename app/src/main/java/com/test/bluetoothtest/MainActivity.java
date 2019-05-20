4001ccb24c42b668ccf597348faa8f25ee60f9ce
Nizam <nizamudeenms@gmail.com>
2019-05-20 18:30:09 +0530

    Notifications while Switch on Bluetooth, Dynamic detection..

--- app/src/main/java/com/test/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/test/bluetoothtest/MainActivity.java
@@ -1,18 +1,30 @@
 package com.test.bluetoothtest;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "notifications";
     Button button;
     int BLUETOOTH_REQUEST = 1;
     BluetoothAdapter bluetoothAdapter;
+    BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +54,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        broadcastReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                final String action = intent.getAction();
+
+                if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                    createNotificationChannel();
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    switch (state) {
+                        case BluetoothAdapter.STATE_OFF:
+                            Toast.makeText(context, "State Off", Toast.LENGTH_SHORT).show();
+                            showNotifications("Bluetooth Off", "Switched Off");
+                            break;
+                        case BluetoothAdapter.STATE_TURNING_OFF:
+                            Toast.makeText(context, "State Turning Off", Toast.LENGTH_SHORT).show();
+                            break;
+                        case BluetoothAdapter.STATE_ON:
+                            showNotifications("Bluetooth On", "Switched ON");
+                            Toast.makeText(context, "State ON", Toast.LENGTH_SHORT).show();
+                            break;
+                        case BluetoothAdapter.STATE_TURNING_ON:
+                            Toast.makeText(context, "State Turning On", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+
+                }
+            }
+        };
+
+        IntentFilter filter1 = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(broadcastReceiver, filter1);
+
     }
 
     @Override
@@ -55,5 +101,50 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Zlikx Channel";
+            String description = "Zlikx Channel Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showNotifications(String title, String body) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+
+        NotificationCompat.Builder noBuilder = new NotificationCompat.Builder(this, "notifications")
+                .setContentTitle(title)
+                .setBadgeIconType(NotificationCompat.BADGE_ICON_LARGE)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(android.R.drawable.stat_sys_data_bluetooth)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentText(body);
+
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(999, noBuilder.build());
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
     }
 }
